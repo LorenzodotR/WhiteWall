9db68ee8397304e625570411008228d96e78d99e
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2024-10-16 15:59:05 +0200

    pages

---
+++ front-end/src/app/pages/contact-chat/contact-chat.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-chat',
+  standalone: true,
+  imports: [],
+  templateUrl: './contact-chat.component.html',
+  styleUrl: './contact-chat.component.scss'
+})
+export class ContactChatComponent {
+
+}
