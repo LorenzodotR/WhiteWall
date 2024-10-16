6b0ee58b6a44a0bb869cc08b8b2d0f930972c963
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2024-10-16 12:27:12 +0200

    server config

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.send('WIP');
 });
 
+app.get('/login', (req, res) => {
+    res.send('Login');
+});
+
+app.get('/contato/:id', (req, res) => {
+    res.send(`Contato ${req.params.id}`);
+});
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
