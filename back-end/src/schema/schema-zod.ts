a7ccd666c1c3283944c6f8d7ddaa98877077ac2c
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2024-10-16 09:54:50 +0200

    schema

--- back-end/src/schema/schema-zod.ts
+++ back-end/src/schema/schema-zod.ts
@@ -1,1 +1,31 @@
 import {z} from 'zod';
+
+export const UserSchema = z.object ({
+    id: z.string(),
+    name: z.string(),
+    email: z.string().email(),
+    age: z.number(),
+    isSubscribed: z.boolean(),
+    createdAt: z.date(),
+    updatedAt: z.date().optional(),
+    deletedAt: z.date().optional(),
+})
+
+export const ChatSchema = z.object ({
+    id: z.string(),
+    message: z.string(),
+    userId: z.string(),
+    recievedAt: z.date(),
+    updatedAt: z.date().optional(),
+    deletedAt: z.date().optional(),    
+})
+
+export const HistoricalChatSchema = z.object ({
+    id: z.string(),
+    message: z.string(),
+    userId: z.string(),
+    recievedAt: z.date(),
+    updatedAt: z.date().optional(),
+    deletedAt: z.date().optional(),
+})
+
