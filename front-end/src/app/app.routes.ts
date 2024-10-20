55d3b4302a89994b6a974be01bfe6d7113f4ccd0
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2024-10-20 11:26:29 -0300

    Routing

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AllChatsComponent } from './pages/all-chats/all-chats.component';
 import { ContactChatComponent } from './pages/contact-chat/contact-chat.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
-    { path: '', component: AllChatsComponent },
+    { path: '', component: LoginComponent },
+    { path: 'all-chats', component: AllChatsComponent },
     { path: 'contact-chat', component: ContactChatComponent },
 ];
